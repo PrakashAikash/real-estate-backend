dcf9c714c74b738d5d44a9f22982474240e18f56
Prakash Kumar Sahni <verpprakashkumar@gmail.com>
2025-08-17 16:39:16 +0530

    Update CORS and backend URLs for deployment

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ app.use(cors({
     'http://localhost:4000',
     'http://localhost:5174',
     'http://localhost:5173',
-    'https://buildestate.vercel.app',
-    'https://real-estate-website-admin.onrender.com',
-    'https://real-estate-website-backend-zfu7.onrender.com',
+    'https://mycityflat.vercel.app',
+    'https://real-estate-admin-self.vercel.app',
+    'https://real-estate-backend-d8kp.onrender.com',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'], // Added HEAD
